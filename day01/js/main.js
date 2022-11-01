fba88b4ff5bed7064fe6fa19c69a6d76affc7eb4
vinferno <vinsonfernandez@gmail.com>
2022-10-31 21:02:39 -0700

    first commit

---
+++ day01/js/main.js
@@ -0,0 +1,72 @@
+
+// this line of code will target the button tagname and add html inside
+document.querySelector('button').innerHTML = 'Hello World!';
+// this line of code will target the button class name and add html inside
+document.querySelector('.btn1').innerHTML = 782 * 459;
+// this line of code will target the button id and add html inside
+// document.querySelector('#myButton').innerHTML = 'This is a cool button';
+
+// target and style the button
+//'background-color: red;'
+document.querySelector('button').style.backgroundColor = 'red';
+
+'font-size: 20px;'
+document.querySelector('.btn1').style.fontSize = '40px';
+
+document.querySelector('#myButton').style.backgroundColor = 'green';
+
+// add an event listener to the button for a click event
+document.querySelector('button').addEventListener('click', function() {
+    // alert('You clicked me!');
+    button.classList.toggle('active');
+});
+
+let button = document.querySelector('button');
+
+button.innerHTML = 'Click Me!';
+
+button.classList.add('active');
+
+
+
+'skeleton-case';
+'kebab-case';
+'snake_case';
+'camelCase';
+'PascalCase';
+'UPPER_CASE'; // this is not a common case
+// skeleton-case;
+// kebab-case;
+let snake_case;
+let camelCase;
+let CamelCase;
+
+
+                  let PascalCase;
+
+let UPPER_CASE;
+
+let upper_case;
+let joe;
+let Joe;
+let JOE;
+
+1+1;
+2-1;
+3*2;
+4/2;
+
+
+// Things you can do with Javascript
+// 1. Change the HTML of a page
+// 2. Change the CSS of a page
+// 3. create Actions to change html or css
+// 4. go to the internet and get data
+
+// actions can be triggered
+// 1. on page load
+// 2. on a users interaction
+// 3. on a timer
+
+// 
+
